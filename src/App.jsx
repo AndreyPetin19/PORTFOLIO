cd2769960bbf08dc97f910d61cb6e50a739f87fb
AndreyPetin19 <petin19ap@gmail.com>
2026-07-02 21:42:18 +0500

    vercel fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import CheckBus from "./CheckBus.jsx";
 import "./index.css";
 import Zeama from "./Zeama.jsx";
-import Projects from "./pages/Projects.jsx";
+import Projects from "./pages/Project.jsx";
 
 function Home() {
   return (
